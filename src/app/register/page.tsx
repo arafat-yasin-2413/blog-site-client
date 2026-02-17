a40f60bfa4029eef14b11b3cbe811616f66f052c
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-02-18 00:23:43 +0600

    refactor: fixes reload issue in nav links

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function SignUpPage() {
+export default function Register() {
     return (
         <div>
-            <h2>Sign Up Page</h2>
+            <h2>Register Page</h2>
         </div>
     );
 }
