1fe07f5764d0e1b68dda8ae41f5db7ef6d1122ac
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-02-20 03:58:03 +0600

    implements multiple routes using group routes

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,9 +1,10 @@
-export const dynamic = "force-dynamic";
+// "use client"
+// export const dynamic = "force-dynamic";
 // search in next js doc : "dynamic"
 
-export default async function AboutPage() {
+export default function AboutPage() {
 
-    await new Promise((resolve)=>setTimeout(resolve, 4000))
+    // await new Promise((resolve)=>setTimeout(resolve, 4000))
 
     // throw new Error("Something Went Wrong");
 
--- src/app/contact/branch/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BranchPage() {
-    return (
-        <div>
-            <h2>This is Branch Page </h2>
-        </div>
-    );
-}
