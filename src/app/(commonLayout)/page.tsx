15655d8cf19d98add2263d93b33be2dd64f12999
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-02-26 03:37:18 +0600

    gets session data in server component

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
 
-    
+    const cookieStore = await cookies();
+    // console.log(cookieStore.get("better-auth.session_token"));
+    console.log(cookieStore.toString());
+
+    const res = await fetch("http://localhost:5000/api/auth/get-session", {
+        headers : {
+            Cookie : cookieStore.toString(),
+        },
+        cache: "no-store",
+    });
+
+    const session = await res.json();
+    console.log(session);
 
     return (
         <div>
