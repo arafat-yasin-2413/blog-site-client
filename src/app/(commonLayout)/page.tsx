3df5ff255869f9e6ed35f11a11c90f863642b550
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-02-26 00:24:10 +0600

    adds better auth

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
 
-export default function Home() {
+export default async function Home() {
+
+    const session = await authClient.getSession();
+    
+    console.log(session);
+
     return (
         <div>
             <Button variant="outline">Click Here</Button>
