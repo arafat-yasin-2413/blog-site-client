68395fc8a3e6bea81f165d86f0c63378b43d3150
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-03-01 06:33:04 +0600

    starts part-4 with dynamic routes

---
+++ src/app/(commonLayout)/blogs/[id]/page.tsx
@@ -0,0 +1,21 @@
+//! This can be used only in client component
+// import { useParams } from "next/navigation";
+
+export default async function BlogPage({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+
+
+
+    return (
+        <div>
+            <h1>This is a dynamic page {id}</h1>
+        </div>
+    );
+}
+
+//! This can be used only in client component
+// const { id } = useParams();
