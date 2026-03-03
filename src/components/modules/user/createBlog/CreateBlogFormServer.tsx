89760a640e7035aa329deb31aa2357647c1798e5
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-03-03 06:03:44 +0600

    implements on demand cache revalidation

--- src/components/modules/user/createBlog/CreateBlogFormServer.tsx
+++ src/components/modules/user/createBlog/CreateBlogFormServer.tsx
@@ -1,17 +1,63 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { env } from "@/env";
+import { revalidateTag, updateTag } from "next/cache";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { toast } from "sonner";
+
+
+const API_URL = env.API_URL;
 
 export default function CreateBlogFormServer() {
-
-    const createBlog = async(formData: FormData)=>{
+    const createBlog = async (formData: FormData) => {
         "use server";
 
-        console.log(formData.get("title"));
+        const title = formData.get("title") as string;
+        const content = formData.get("content") as string;
+        const tags = formData.get("tags") as string;
 
-    }
+        const blogData = {
+            title,
+            content,
+            tags: tags.split(",").map(item=> item.trim()).filter(item=>item !== ""),
+        };
+
+        // console.log(JSON.stringify(blogData));
+
+        const cookieStore = await cookies()
+
+        const res = await fetch(`${API_URL}/posts`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Cookie: cookieStore.toString(),
+            },
+            body: JSON.stringify(blogData),
+        });
+
+        // console.log(res);
+        // toast.success("Post Created");
+        
+        if(res.status) {
+            redirect("/dashboard/create-blog?success")
+        }
+
+        if(res.ok) {
+            revalidateTag("blogPosts", "max");
+            // updateTag("blogPosts"); // use either one of them
+        }
+    };
 
     return (
         <Card className="max-w-2xl mx-auto">
@@ -23,24 +69,40 @@ export default function CreateBlogFormServer() {
             <CardContent>
                 <form id="blog-form" action={createBlog}>
                     <FieldGroup>
+                        {/* title field */}
                         <Field>
-                            <FieldLabel>Title</FieldLabel>
-                            <Input type="text" name="title"></Input>
+                            <FieldLabel htmlFor="title">Title</FieldLabel>
+                            <Input
+                                id="title"
+                                name="title"
+                                placeholder="Blog Title"
+                                required></Input>
                         </Field>
+                        {/* content field */}
                         <Field>
-                            <FieldLabel>Content</FieldLabel>
-                            <Input type="text" name="title"></Input>
+                            <FieldLabel htmlFor="content">Content</FieldLabel>
+                            <Textarea
+                                id="content"
+                                name="content"
+                                placeholder="Write your blog content"
+                                required></Textarea>
                         </Field>
+                        {/* tags field */}
                         <Field>
-                            <FieldLabel>Tags</FieldLabel>
-                            <Input type="text" name="title"></Input>
+                            <FieldLabel htmlFor="tags">Tags</FieldLabel>
+                            <Input
+                                id="tags"
+                                name="tags"
+                                placeholder="nextjs, web"></Input>
                         </Field>
                     </FieldGroup>
                 </form>
             </CardContent>
 
             <CardFooter>
-                <Button form="blog-form" type="submit" className="w-full">Submit</Button>
+                <Button form="blog-form" type="submit" className="w-full">
+                    Submit
+                </Button>
             </CardFooter>
         </Card>
     );
